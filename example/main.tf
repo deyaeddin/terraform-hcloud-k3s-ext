f648b33e8547cc10f4571ed8c6b2622da4b8b52b
DeyaEddin <deya@gmx.com>
2021-06-03 21:05:01 +0300

    update: adding example

---
+++ example/main.tf
@@ -0,0 +1,14 @@
+
+module "k3s-ext" {
+  source  = "deyaeddin/k3s-ext/hcloud"
+  version = "0.0.1"
+  cloud_flare_api_email = ""
+  cloud_flare_api_key = ""
+  cloud_flare_api_token = ""
+  default_domain = ""
+  hcloud_dns_api_token = ""
+  hcloud_masters_extra_scripts = []
+  hcloud_node_extra_scripts = []
+  hcloud_token = ""
+  issuer_email = ""
+}
